e624b4f4547b476b5a33ebf95bcfcc9b85c2e544
BrainHunter <mail-brainhunter@nota.de>
2020-10-04 20:04:44 +0200

    Fixed PLL selection in discovery audio code

--- Drivers/STM32F4-Discovery/stm32f4_discovery_audio.c
+++ Drivers/STM32F4-Discovery/stm32f4_discovery_audio.c
@@ -499,7 +499,7 @@ __weak void BSP_AUDIO_OUT_ClockConfig(I2S_HandleTypeDef *hi2s, uint32_t AudioFre
   /* Enable PLLI2S clock */
   HAL_RCCEx_GetPeriphCLKConfig(&rccclkinit);
   /* PLLI2S_VCO Input = HSE_VALUE/PLL_M = 1 Mhz */
-  if ((freqindex & 0x7) == 0)
+  if ((freqindex & 0x7) != 0) // was ==
   {
     /* I2S clock config 
     PLLI2S_VCO = f(VCO clock) = f(PLLI2S clock input) � (PLLI2SN/PLLM)
